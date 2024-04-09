81140e65fb2ec97e6cfe88cd6558d26d4d5f3e64
Kailash Prem <kailash.prem@iinerds.com>
2024-04-09 17:05:22 +0530

    samples

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ import numpy as np
 
 
 def main():
-    img1 = cv2.imread("patch_1.img")
-    img2 = cv2.imread("patch_2.img")
+    img1 = cv2.imread("patch_1.png")
+    img2 = cv2.imread("patch_2.png")
     merged_img = np.concatenate((img1, img2), axis=1)
     cv2.imwrite("merged.png", merged_img)
 
